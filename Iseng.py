8bdd1c74d7d82952fda4d75e7f8c6fc0d6a7622b
Yohanes Arya <zerostormproductions@gmail.com>
2024-03-16 10:40:18 +0700

    testing2

--- Iseng.py
+++ Iseng.py
@@ -1,3 +1,20 @@
 import openai
 
-# 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
+openai.api_key = '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'
+
+messages = [ {"role": "system", "content": "You are a intelligent assistant."} ]
+
+ 
+while True: 
+    message = input("User : ") 
+    if message: 
+        messages.append( 
+            {"role": "user", "content": message}, 
+        ) 
+        chat = openai.ChatCompletion.create( 
+            model="gpt-3.5-turbo", messages=messages 
+        ) 
+      
+    reply = chat.choices[0].message.content 
+    print(f"ChatGPT: {reply}") 
+    messages.append({"role": "assistant", "content": reply})
